b315bbd18567172296e6f451d4c9851736bfb100
raymonraymon <raymond03152410@163.com>
2016-06-26 10:47:01 +0800

    update with ssh pub key

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <iostream>
-
+using namespace std;
 int main()
 {
     printf("Hello world!\n");                   // 教科书的写法
     puts("Hello world!\n");                     // 我最喜欢的
     puts("Hello" " " "world!");               // 拼接字符串
-    std::cout << "Hello world!" << std::endl; // C++风格的教科书写法
+    cout << "Hello world!" << endl; // C++风格的教科书写法
 
     return 0;
 }
