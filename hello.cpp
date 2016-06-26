f5bee663710dba2eb2b8c1f7126988839aa78e6e
raymonraymon <raymond03152410@163.com>
2016-06-26 10:44:29 +0800

    test ssh key

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    printf("Hello world!");                   // 教科书的写法
-    puts("Hello world!");                     // 我最喜欢的
+    printf("Hello world!\n");                   // 教科书的写法
+    puts("Hello world!\n");                     // 我最喜欢的
     puts("Hello" " " "world!");               // 拼接字符串
     std::cout << "Hello world!" << std::endl; // C++风格的教科书写法
 
